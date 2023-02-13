3bc42536df712aa8c1f62bf40d0ec6d2b4100e24
Lucas Alves <lucasalves@MacBook-Air-de-Lucas.local>
2023-02-13 11:30:41 -0300

    First commit

---
+++ src/main/java/com/lucasalvess/sqslocalstack/SqsLocalstackApplication.java
@@ -0,0 +1,13 @@
+package com.lucasalvess.sqslocalstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsLocalstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsLocalstackApplication.class, args);
+	}
+
+}
