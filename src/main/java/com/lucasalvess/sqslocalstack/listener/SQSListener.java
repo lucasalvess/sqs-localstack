a1c8680c36cc1bcf104159d4e371937e7b002093
Lucas Alves <lucasalves@MacBook-Air-de-Lucas.local>
2023-02-20 12:59:39 -0300

    Adding dlq listener test

--- src/main/java/com/lucasalvess/sqslocalstack/listener/SQSListener.java
+++ src/main/java/com/lucasalvess/sqslocalstack/listener/SQSListener.java
@@ -2,15 +2,24 @@ package com.lucasalvess.sqslocalstack.listener;
 
 import com.lucasalvess.sqslocalstack.dto.MessageDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class SQSListener {
 
-        @SqsListener("${cloud.aws.queue.name}")
-        public void receiveMessage(MessageDTO message) {
+        @SqsListener(value = "${cloud.aws.queue.name}", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+        public void receiveMessage(MessageDTO message, @Header("ApproximateReceiveCount") final int receiveCount) {
             log.info("SQS Message Received : {}", message);
+            log.info("Attempt count : {}", receiveCount);
+            throw new RuntimeException("Ops, an error occurs");
+        }
+
+        @SqsListener("${cloud.aws.dlq.name}")
+        public void receiveDLQMessage(String message) {
+            log.info("DLQ Message Received : {}", message);
         }
 }
